b09f1e40c1c37c22790e213cfff9c97e23113ca1
Whale Well Well <ashereverettdev@gmail.com>
2022-04-30 10:44:21 -0400

    Added throwFatal() and throwMinor() functions

--- Logger.js
+++ Logger.js
@@ -1,6 +1,14 @@
 // Logger.js by WhaleWellWell. Free to use! Have fun!
 
 let Errors = {
-  "Fatal" : "A FATAL error has occurred :(",
-  "Minor" : "A Minor error has orrurred"
+  "Fatal": "A FATAL error has occurred :( ",
+  "Minor": "A Minor error has orrurred "
 };
+
+function throwFatal(message) {
+  console.log(Errors.Fatal + message);
+}
+
+function throwMinor(message) {
+  console.log(Errors.Minor + message);
+}
