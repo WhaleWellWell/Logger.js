57040553cbea8e4c18916a90e6dfd84aec0ced55
Whale Well Well <ashereverettdev@gmail.com>
2022-05-08 18:20:09 -0400

    Added return statements and logMessage() function

--- Logger.js
+++ Logger.js
@@ -6,9 +6,16 @@ let Errors = {
 };
 
 function throwFatal(message) {
-  console.log(Errors.Fatal + message);
+  console.log(Errors.Fatal + message);]
+  return message;
 }
 
 function throwMinor(message) {
   console.log(Errors.Minor + message);
+  return message;
+}
+
+function logMessage(message) {
+  console.log(message);
+  return message;
 }
